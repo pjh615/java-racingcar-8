f2d52886b71081833c8457b3f09dae01457be6b4
Junha Park <poiu7825@pusan.ac.kr>
2025-10-26 23:05:05 +0900

    feat: add main application logic

--- src/main/java/racingcar/Application.java
+++ src/main/java/racingcar/Application.java
@@ -1,7 +1,25 @@
 package racingcar;
 
+import racingcar.controller.GameController;
+import racingcar.service.RaceService;
+import racingcar.util.Converter;
+import racingcar.util.Parser;
+import racingcar.util.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
+        InputView inputView = new InputView();
+        OutputView outputView = new OutputView();
+        Parser parser = new Parser();
+        Validator validator = new Validator();
+        Converter converter = new Converter();
+        RaceService raceService = new RaceService();
+
+        GameController gameController = new GameController(
+                inputView, outputView, parser, validator, converter, raceService
+        );
+        gameController.gameStart();
     }
 }
