c104662025f536e89cd05c4fdf8cad0155cea1d1
Junha Park <poiu7825@pusan.ac.kr>
2025-10-26 23:08:40 +0900

    feat: add GameController to manage input, output and game progress

---
+++ src/main/java/racingcar/controller/GameController.java
@@ -0,0 +1,46 @@
+package racingcar.controller;
+
+import racingcar.service.RaceService;
+import racingcar.util.Converter;
+import racingcar.util.Parser;
+import racingcar.util.Validator;
+import racingcar.view.InputView;
+import racingcar.view.OutputView;
+
+import java.util.List;
+
+public class GameController {
+    public final InputView inputView;
+    public final OutputView outputView;
+    public final Parser parser;
+    public final Validator validator;
+    public final Converter converter;
+    public final RaceService raceService;
+
+    public GameController(InputView inputView, OutputView outputView, Parser parser,
+                          Validator validator, Converter converter, RaceService raceService) {
+        this.inputView = inputView;
+        this.outputView = outputView;
+        this.parser = parser;
+        this.validator = validator;
+        this.converter = converter;
+        this.raceService = raceService;
+    }
+
+    public void gameStart() {
+        String carNamesInput = inputView.inputCarNames();
+        List<String> carNames = parser.parseByDelimiter(carNamesInput);
+
+        String attemptCountInput = inputView.inputAttemptCount();
+        int attemptCount = converter.stringToInt(attemptCountInput);
+
+        outputView.displayStart();
+        raceService.init(carNames);
+        for (int i = 0; i < attemptCount; i++) {
+            raceService.playRace();
+            outputView.displayResultByOrder(raceService.getRaceResult());
+        }
+        outputView.displayWinner(raceService.getWinnerResult());
+
+    }
+}
