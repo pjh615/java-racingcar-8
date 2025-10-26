9ab0d880dd57973cf0488ddaa7fb4077b9689fe6
Junha Park <poiu7825@pusan.ac.kr>
2025-10-26 23:11:33 +0900

    feat: add output logic to OutputView

---
+++ src/main/java/racingcar/view/OutputView.java
@@ -0,0 +1,16 @@
+package racingcar.view;
+
+public class OutputView {
+
+    public void displayStart() {
+        System.out.println("실행 결과");
+    }
+
+    public void displayResultByOrder(String progress) {
+        System.out.println(progress);
+    }
+
+    public void displayWinner(String winners) {
+        System.out.println("최종 우승자: " + winners);
+    }
+}
